23a24eef4d997a698636e5f86e5413fac65fe2ee
Lauren Maurer <lauren@openmindplatform.org>
2022-01-12 19:10:48 -0500

    log correct port to console for debugging

--- src/server/server.js
+++ src/server/server.js
@@ -2,4 +2,4 @@ const app = require("../client/js/app");
 
 // Setup Server
 const port = 3000;
-const server = app.listen(process.env.PORT || port, () => {console.log(`Server running on localhost: ${port}`)});
+const server = app.listen(process.env.PORT || port, () => {console.log(`Server running on port ${process.env.PORT || port}`)});
